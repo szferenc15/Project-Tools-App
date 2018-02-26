deb13e24b8c559f7dde8225282d5153073a52955
Szendrei Ferenc <szendrei.ferenc15@gmail.com>
2018-02-26 03:18:43 +0100

    data.sql utf-8 to ascii

--- sport_mates/src/main/resources/data.sql
+++ sport_mates/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 -- User data:
 INSERT INTO user(id, first_name, last_name, username, password, email, phone_number, city, birth_date, is_male) 
-        VALUES (1, 'Kertész', 'Kászon', 'kaszon', 'Dr123$,.-', 'kaszon@gmail.com', '06302003000', 'Budapest', '1990-01-01', true);
+        VALUES (1, 'Kertesz', 'Kaszon', 'kaszon', 'Dr123$,.-', 'kaszon@gmail.com', '06302003000', 'Budapest', '1990-01-01', true);
 INSERT INTO user(id, first_name, last_name, username, password, email, phone_number, city, birth_date, is_male) 
-        VALUES (2, 'Polozgai', 'Máté', 'mate', 'Dr123$,.-', 'mate@gmail.com', '06303004000', 'Budapest', '1990-01-02', true);
+        VALUES (2, 'Polozgai', 'Mate', 'mate', 'Dr123$,.-', 'mate@gmail.com', '06303004000', 'Budapest', '1990-01-02', true);
 INSERT INTO user(id, first_name, last_name, username, password, email, phone_number, city, birth_date, is_male) 
         VALUES (3, 'Szebenyi', 'Gergely', 'geri', 'Dr123$,.-', 'geri@gmail.com', '06304005000', 'Budapest', '1990-01-03', true);
 INSERT INTO user(id, first_name, last_name, username, password, email, phone_number, city, birth_date, is_male) 
@@ -10,33 +10,33 @@ INSERT INTO user(id, first_name, last_name, username, password, email, phone_num
 
 -- Event data:
 INSERT INTO event(id, organizer, name, locale, price, date_of_event, start, finish, headcount, audience, description) 
-        VALUES (1, 'kaszon', 'Spirituális hegymászás', 'Japán, Kitayama, Fuji', 0, '2018-06-07', '09:00:00', '13:00:00', 
-                2, 'férfi', 'A Japánban lévő szent hegy, a Fuji megmászása.');
+        VALUES (1, 'kaszon', 'Spiritualis hegymaszas', 'Japan, Kitayama, Fuji', 0, '2018-06-07', '09:00:00', '13:00:00', 
+                2, 'ferfi', 'A Japanban levő szent hegy, a Fuji megmaszasa.');
 INSERT INTO event(id, organizer, name, locale, price, date_of_event, start, finish, headcount, audience, description) 
         VALUES (2, 'geri', 'Flúgos futam', 'Kalifornia, Joshua Tree Nemzeti Park', 0, '2018-06-05', '13:00:00', '18:00:00', 
-                2, 'férfi', 'Lábnap a Joshua Tree Nemzeti Parkban.');
+                2, 'ferfi', 'Labnap a Joshua Tree Nemzeti Parkban.');
         
 -- Comment data:
 INSERT INTO comment(id, message, event_id, user_id) 
-        VALUES (1, 'Azt mondják elég jó onnan a kilátás. Hamarosan megtudjuk!', 1, 1);
+        VALUES (1, 'Azt mondjak eleg jo onnan a kilatas. Hamarosan megtudjuk!', 1, 1);
 INSERT INTO comment(id, message, event_id, user_id) 
-        VALUES (2, 'Japán... érdekes hely, ideje felfedezni!', 1, 2);
+        VALUES (2, 'Japan... erdekes hely, ideje felfedezni!', 1, 2);
 INSERT INTO comment(id, message, event_id, user_id) 
-        VALUES (3, 'Huh, ezt már nagyon várom!', 2, 3);
+        VALUES (3, 'Huh, ezt mar nagyon varom!', 2, 3);
 INSERT INTO comment(id, message, event_id, user_id) 
-        VALUES (4, 'Végre egy kis edzes! 5 óra futás!', 2, 4);
+        VALUES (4, 'Vegre egy kis edzes! 5 ora futas!', 2, 4);
 
 -- SportCategory data:
 INSERT INTO sport_category(id, category) 
-        VALUES (1, 'Hegymászás');
+        VALUES (1, 'Hegymaszas');
 INSERT INTO sport_category(id, category) 
-        VALUES (2, 'Futás');
+        VALUES (2, 'Futas');
 
 -- EventSportCategory data:
 INSERT INTO event_sport_category(id, category, event_id) 
-        VALUES (1, 'Hegymászás', 1);
+        VALUES (1, 'Hegymaszas', 1);
 INSERT INTO event_sport_category(id, category, event_id) 
-        VALUES (2, 'Futás', 2);
+        VALUES (2, 'Futas', 2);
 
 -- User-Event join table data:
 INSERT INTO user_event(user_id, event_id) 
